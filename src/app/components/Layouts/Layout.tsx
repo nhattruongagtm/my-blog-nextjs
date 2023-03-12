e0c1a752ae9cad7f05994f2cab1ab6e2d3273090
Truong Nhat Huynh <o_truong_nhat.h@kbtg.tech>
2023-03-12 20:04:39 +0700

    fixed ads didnt display

--- src/app/components/Layouts/Layout.tsx
+++ src/app/components/Layouts/Layout.tsx
@@ -12,12 +12,11 @@ const Layout = ({ children }: Props) => {
     <div className="dark:bg-bg-dark pb-2 p-5 lg:p-0 lg:pb-2">
       <Header />
       <div className="min-h-[600px]">{children}</div>
-      <div className="ads mb-[80px] flex justify-center rounded-lg">
+      <div className="ads mb-[80px] rounded-lg">
         <GoogleAd
           googleAdId="ca-pub-5749665502208213"
           slot="2268403632"
           timeout={0}
-          style={{ height: "100px", display: "block" }}
         />
       </div>
       <Footer />
